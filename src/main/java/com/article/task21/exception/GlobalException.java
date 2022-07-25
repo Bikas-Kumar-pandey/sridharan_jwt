7137eb47eb8f8edabad4166786f9f5332686eb50
Sridharan <sridharan@divum.in>
2022-07-25 17:27:07 +0530

    refactor

--- src/main/java/com/article/task21/exception/GlobalException.java
+++ src/main/java/com/article/task21/exception/GlobalException.java
@@ -1,6 +1,5 @@
 package com.article.task21.exception;
 
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
